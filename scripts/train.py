c3caedb4f27cbbc1fdd9f6269cea5a63e543c035
Anhthy Ngo <an3056@nyu.edu>
2020-11-02 21:56:37 -0500

    train file

---
+++ scripts/train.py
@@ -0,0 +1,54 @@
+def get_full_filepath(path, enc_type):
+    filename = 'nmt_enc_'+enc_type+'_dec_rnn.pth'
+    return os.path.join(path, filename)
+
+def save_models(nmt_model, path, enc_type):
+    if not os.path.exists(path):
+            os.makedirs(path)
+    filename = 'nmt_enc_'+enc_type+'_dec_rnn.pth'
+    torch.save(nmt_model, os.path.join(path, filename))
+
+def train_model(dataloader, nmt, num_epochs=50, val_every=1, saved_model_path = '.', enc_type ='rnn'):
+
+    best_bleu = -1;
+    for epoch in range(num_epochs):
+
+        start = time.time()
+        running_loss = 0
+
+        print('Epoch: [{}/{}]'.format(epoch, num_epochs));
+        
+        for i, data in notebook.tqdm(enumerate(dataloader['train']), total=len(dataloader['train'])):  
+            _, curr_loss = nmt.train_step(data);
+            running_loss += curr_loss
+
+        epoch_loss = running_loss / len(dataloader['train']) 
+        
+        print("epoch {} loss = {}, time = {}".format(epoch, epoch_loss,
+                                                        time.time() - start))
+        sys.stdout.flush()
+   
+        if epoch%val_every == 0:
+            val_bleu_score = nmt.get_bleu_score(dataloader['val']);
+            print('validation bleu: ', val_bleu_score)
+            sys.stdout.flush()
+            
+            nmt.scheduler_step(val_bleu_score);
+            
+            if val_bleu_score > best_bleu:
+                best_bleu = val_bleu_score
+                save_models(nmt, saved_model_path, enc_type);
+
+        print('='*50)
+
+    print("Training completed. Best BLEU is {}".format(best_bleu))
+
+train_again = False
+if os.path.exists(get_full_filepath(saved_models_dir, 'rnn')) and (not train_again):
+    nmt_rnn = torch.load(get_full_filepath(saved_models_dir, 'rnn'), map_location=global_variables.device)
+else:
+    train_model(dataloader_dict, nmt_rnn, 
+                      num_epochs = num_epochs, 
+                      saved_model_path = saved_models_dir, 
+                      enc_type = 'rnn_test')
+    

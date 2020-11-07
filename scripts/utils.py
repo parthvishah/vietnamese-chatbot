9c7e9030841b488345256e472186f5c878dee79c
Anhthy Ngo <an3056@nyu.edu>
2020-11-07 14:52:00 -0500

    update utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -31,35 +31,35 @@ def train_model(dataloader, nmt, num_epochs=50, val_every=1, saved_model_path = 
 	best_bleu = -1;
 	for epoch in range(num_epochs):
 
-	start = time.time()
-	running_loss = 0
+		start = time.time()
+		running_loss = 0
 
-	print('Epoch: [{}/{}]'.format(epoch, num_epochs));
+		print('Epoch: [{}/{}]'.format(epoch, num_epochs));
 
-	for i, data in enumerate(dataloader['train']):
-		_, curr_loss = nmt.train_step(data);
-		running_loss += curr_loss
+		for i, data in enumerate(dataloader['train']):
+			_, curr_loss = nmt.train_step(data);
+			running_loss += curr_loss
 
-	epoch_loss = running_loss / len(dataloader['train'])
+		epoch_loss = running_loss / len(dataloader['train'])
 
-	print("epoch {} loss = {}, time = {}".format(epoch, epoch_loss,
-													time.time() - start))
-	log.info(f"epoch {epoch} | loss {epoch_loss} |time = {time.time() - start}")
+		print("epoch {} loss = {}, time = {}".format(epoch, epoch_loss,
+														time.time() - start))
+		log.info(f"epoch {epoch} | loss {epoch_loss} |time = {time.time() - start}")
 
 
-	sys.stdout.flush()
-
-	if epoch%val_every == 0:
-		val_bleu_score = nmt.get_bleu_score(dataloader['dev']);
-		print('validation bleu: ', val_bleu_score)
 		sys.stdout.flush()
 
-		nmt.scheduler_step(val_bleu_score);
+		if epoch%val_every == 0:
+			val_bleu_score = nmt.get_bleu_score(dataloader['dev']);
+			print('validation bleu: ', val_bleu_score)
+			sys.stdout.flush()
 
-		if val_bleu_score > best_bleu:
-			best_bleu = val_bleu_score
-			save_models(nmt, saved_model_path, enc_type);
+			nmt.scheduler_step(val_bleu_score);
 
-	print('='*50)
+			if val_bleu_score > best_bleu:
+				best_bleu = val_bleu_score
+				save_models(nmt, saved_model_path, enc_type);
+
+		print('='*50)
 
 	print("Training completed. Best BLEU is {}".format(best_bleu))

a2867b9183d2062ec5d15d89b5db5adc31c0a4a4
Ant Ngo <an3056@nyu.edu>
2020-11-08 17:33:56 -0500

    updates

--- scripts/train.py
+++ scripts/train.py
@@ -1,134 +1,22 @@
-import os
-import time
-import torch
-from torch.utils.data import DataLoader
-#from tqdm import notebook
-#from tqdm import tdqm
-from functools import partial
-import sys
-# custom modules
-import global_variables
-import nmt_dataset
-import nnet_models_new
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=v100_sxm2_4,p40_4,p100_4,k80_4
+#SBATCH --mem=64000
+#SBATCH --time=48:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=an3056@nyu.edu
+#SBATCH --job-name="seq2seq_train"
+#SBATCH --output=/scratch/an3056/nlp_project/outputs/%j.out
 
+module purge
+module load anaconda3/5.3.1
+module load cuda/10.0.130
+module load gcc/6.3.0
 
-# set file paths
-source_name = 'en'
-target_name = 'vi'
-
-base_saved_models_dir = '/scratch/an3056/nlp_project/saved_models/'
-saved_models_dir = os.path.join(base_saved_models_dir, source_name+'2'+target_name)
-
-main_data_path = '/scratch/an3056/nlp_project/vietnamese-chatbot/data/interim/iwslt15-en-vn/'
-
-path_to_train_data = {'source':main_data_path+'train.'+source_name, 
-                      'target':main_data_path+'train.'+target_name}
-path_to_dev_data = {'source': main_data_path+'dev.'+source_name, 
-                      'target':main_data_path+'dev.'+target_name}
-
-saved_language_model_dir = os.path.join(saved_models_dir, 'lang_obj')
-
-dataset_dict = {'train': nmt_dataset.LanguagePair(source_name = source_name, target_name=target_name, 
-                    filepath = path_to_train_data, 
-                    lang_obj_path = saved_language_model_dir,
-                     minimum_count = 1), 
-
-                'dev': nmt_dataset.LanguagePair(source_name = source_name, target_name=target_name, 
-                    filepath = path_to_dev_data, 
-                    lang_obj_path = saved_language_model_dir,
-                    minimum_count = 1)}
-
-MAX_LEN = int(dataset_dict['train'].main_df['source_len'].quantile(0.9999))
-batchSize = 64
-
-dataloader_dict = {'train': DataLoader(dataset_dict['train'], batch_size = batchSize, 
-                            collate_fn = partial(nmt_dataset.vocab_collate_func, MAX_LEN=MAX_LEN),
-                            shuffle = True, num_workers=0), 
-                    'dev': DataLoader(dataset_dict['dev'], batch_size = batchSize, 
-                            collate_fn = partial(nmt_dataset.vocab_collate_func, MAX_LEN=MAX_LEN),
-                            shuffle = True, num_workers=0) }
-
-# Configuration
-source_lang_obj = dataset_dict['train'].source_lang_obj
-target_lang_obj = dataset_dict['train'].target_lang_obj
-
-source_vocab = dataset_dict['train'].source_lang_obj.n_words;
-target_vocab = dataset_dict['train'].target_lang_obj.n_words;
-hidden_size = 512
-rnn_layers = 1
-lr = 0.25;
-longest_label = 1;
-gradient_clip = 0.3;
-use_cuda = True
-
-num_epochs = 10
-
-encoder_rnn = nnet_models_new.EncoderRNN(input_size = source_vocab,
-                         hidden_size = hidden_size,
-                         numlayers = rnn_layers)
-
-decoder_rnn = nnet_models_new.DecoderRNN(target_vocab, hidden_size, rnn_layers)
-
-nmt_rnn = nnet_models_new.seq2seq(encoder_rnn, decoder_rnn,
-                              lr = lr, 
-                              use_cuda = use_cuda, 
-                              hiddensize = hidden_size, 
-                              numlayers = hidden_size, 
-                              target_lang=dataset_dict['train'].target_lang_obj,
-                              longest_label = longest_label,
-                              clip = gradient_clip)
-
-def get_full_filepath(path, enc_type):
-    filename = 'nmt_enc_'+enc_type+'_dec_rnn.pth'
-    return os.path.join(path, filename)
-
-def save_models(nmt_model, path, enc_type):
-    if not os.path.exists(path):
-            os.makedirs(path)
-    filename = 'nmt_enc_'+enc_type+'_dec_rnn.pth'
-    torch.save(nmt_model, os.path.join(path, filename))
-
-def train_model(dataloader, nmt, num_epochs=50, val_every=1, saved_model_path = '.', enc_type ='rnn'):
-
-    best_bleu = -1;
-    for epoch in range(num_epochs):
-
-        start = time.time()
-        running_loss = 0
-
-        print('Epoch: [{}/{}]'.format(epoch, num_epochs));
-        
-        for i, data in enumerate(dataloader['train']):  
-            _, curr_loss = nmt.train_step(data);
-            running_loss += curr_loss
-
-        epoch_loss = running_loss / len(dataloader['train']) 
-        
-        print("epoch {} loss = {}, time = {}".format(epoch, epoch_loss,
-                                                        time.time() - start))
-        sys.stdout.flush()
-   
-        if epoch%val_every == 0:
-            val_bleu_score = nmt.get_bleu_score(dataloader['dev']);
-            print('validation bleu: ', val_bleu_score)
-            sys.stdout.flush()
-            
-            nmt.scheduler_step(val_bleu_score);
-            
-            if val_bleu_score > best_bleu:
-                best_bleu = val_bleu_score
-                save_models(nmt, saved_model_path, enc_type);
-
-        print('='*50)
-
-    print("Training completed. Best BLEU is {}".format(best_bleu))
-
-train_again = False
-if os.path.exists(get_full_filepath(saved_models_dir, 'rnn')) and (not train_again):
-    nmt_rnn = torch.load(get_full_filepath(saved_models_dir, 'rnn'), map_location=global_variables.device)
-else:
-    train_model(dataloader_dict, nmt_rnn, 
-                      num_epochs = num_epochs, 
-                      saved_model_path = saved_models_dir, 
-                      enc_type = 'rnn_test')
-    
+NETID=an3056
+cd /scratch/an3056/nlp_project/vietnamese-chatbot/scripts/
+source activate nmt_env
+python train.py

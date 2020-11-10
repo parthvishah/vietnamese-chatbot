b72d3356c2e985b2134b58abedd7ac3ac9fa3bc0
Anhthy Ngo <an3056@nyu.edu>
2020-11-10 12:34:55 -0500

    update no attention script

--- scripts/train_no_attention.py
+++ scripts/train_no_attention.py
@@ -48,7 +48,7 @@ def main():
 	if torch.cuda.is_available():
 		torch.cuda.manual_seed_all(parser.seed)
 	log.info("The seed is {}".format(parser.seed))
-	
+
 	# set file paths
 	source_name = parser.source_name
 	target_name = parser.target_name
@@ -56,6 +56,8 @@ def main():
 	# get saved models dir
 	base_saved_models_dir = parser.save_dir
 	saved_models_dir = os.path.join(base_saved_models_dir, source_name+'2'+target_name)
+
+	log.info("We will save the models in this directory: {}".format(saved_models_dir))
 
 	# get data dir
 	main_data_path = parser.data_dir
@@ -67,7 +69,7 @@ def main():
 	# get dictionary of datasets
 	dataset_dict = {'train': nmt_dataset.LanguagePair(source_name = source_name, target_name=target_name, filepath = path_to_train_data, lang_obj_path = saved_language_model_dir, minimum_count = 1), 'dev': nmt_dataset.LanguagePair(source_name = source_name, target_name=target_name, filepath = path_to_dev_data, lang_obj_path = saved_language_model_dir, minimum_count = 1)}
 
-	# get max sentence length bby 99% percentile
+	# get max sentence length by 99% percentile
 	MAX_LEN = int(dataset_dict['train'].main_df['source_len'].quantile(0.9999))
 	batchSize = parser.batch_size
 	log.info("Batch size = {}.".format(batchSize))
@@ -97,12 +99,17 @@ def main():
 
 	log.info("Seq2Seq Model with the following parameters: batch_size = {}, learning_rate = {}, hidden_size = {}, rnn_layers = {}, lr = {}, longest_label = {}, gradient_clip = {}, num_epochs = {}, source_name = {}, target_name = {}".format(batchSize, lr, hidden_size, rnn_layers, lr, longest_label, gradient_clip, num_epochs, source_name, target_name))
 
+	# do we want to train again?
 	train_again = False
 
+	# check if there is a saved model and if we want to train again
 	if os.path.exists(utils.get_full_filepath(saved_models_dir, 'rnn')) and (not train_again):
+		log.info("Retrieving saved model from {}".format(utils.get_full_filepath(saved_models_dir, 'rnn')))
 		nmt_rnn = torch.load(utils.get_full_filepath(saved_models_dir, 'rnn'), map_location=global_variables.device)
+	# train model again
 	else:
-		log.info("Starting to train...")
+		log.info("Check if this path exists: {}".format(utils.get_full_filepath(saved_models_dir, 'rnn')))
+		log.info("It does not exist! Starting to train...")
 		utils.train_model(dataloader_dict, nmt_rnn,num_epochs = num_epochs, saved_model_path = saved_models_dir, enc_type = 'rnn_test')
 	log.info("Total time is: {} min : {} s".format((time.time()-start)//60, (time.time()-start)%60))
 

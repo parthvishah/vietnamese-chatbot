a014965757be4d9e0cecae10020de42f55ddaede
Parthvi Shah <shahparthvi22@gmail.com>
2020-11-14 11:53:21 -0500

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,6 +7,13 @@ import sys
 import logging as log
 from datetime import datetime as dt
 import time
+import copy
+import numpy as np
+from collections import namedtuple
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+%matplotlib inline
 
 def get_full_filepath(path, enc_type):
 	'''
@@ -61,3 +68,87 @@ def train_model(dataloader, nmt, num_epochs=50, val_every=1, saved_model_path = 
 		print('='*50)
 
 	print("Training completed. Best BLEU is {}".format(best_bleu))
+
+
+def get_binned_bl_score(nmt_model, val_dataset):
+    
+#     source_len = np.sort(np.array(val_dataset.main_df['source_len']))
+#     len_threshold = [0]+[source_len[x*1000+376] for x in range(1, 21)];
+#     len_threshold = np.unique(len_threshold)
+    len_threshold = np.arange(0, 31, 5)
+    bin_bl_score = np.zeros(len(len_threshold));
+    
+    for i in notebook.tqdm(range(1, len(len_threshold)), total = len(len_threshold)-1):
+        min_len = len_threshold[i-1]
+#         min_len = 0
+        max_len = len_threshold[i]
+        
+        temp_dataset = copy.deepcopy(val_dataset);
+        temp_dataset.main_df = temp_dataset.main_df[(temp_dataset.main_df['source_len'] > min_len) & (temp_dataset.main_df['source_len'] <= max_len)];
+        temp_loader = DataLoader(temp_dataset, batch_size = batchSize, 
+                            collate_fn = partial(nmt_dataset.vocab_collate_func, MAX_LEN=100),
+                            shuffle = True, num_workers=0)
+        
+        bin_bl_score[i] = nmt_model.get_bleu_score(temp_loader);
+        
+    
+    len_threshold = len_threshold[1:]
+    bin_bl_score = bin_bl_score[1:]
+    
+    plt.plot(len_threshold, bin_bl_score, 'x-')
+    plt.ylim(0, np.max(bin_bl_score)+1)
+    plt.xlabel('len')
+    plt.ylabel('bl score')
+    
+    return len_threshold, bin_bl_score
+    
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions, cmap='bone', aspect='auto')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       [global_variables.EOS_TOKEN], rotation=90)
+    ax.set_yticklabels([''] + output_words.split(' ')+
+                       [global_variables.EOS_TOKEN]);
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+def get_encoded_batch(sentence, lang_obj, use_cuda):
+    """ accepts only bsz = 1.
+        input: one sentence as a string
+        output: named tuple with vector and length"""
+    
+    sentence = sentence + ' ' + global_variables.EOS_TOKEN;
+    tensor = source_lang_obj.txt2vec(sentence).unsqueeze(0)
+    
+    device = torch.device('cuda') if use_cuda and torch.cuda.is_available() else torch.device('cpu');
+    
+    named_returntuple = namedtuple('namedtuple', ['text_vecs', 'text_lens', 'label_vecs', 'label_lens', 'use_packed'])
+    return_tuple = named_returntuple( tensor.to(device), 
+                                     torch.from_numpy(np.array([tensor.shape[-1]])).to(device),
+                                     None,
+                                     None,
+                                     False );
+
+    return return_tuple
+
+def get_translation(nmt_model, sentence, lang_obj, use_cuda):
+    print('souce: ', sentence)
+    batch = get_encoded_batch(sentence, lang_obj, use_cuda);
+    prediction, attn_scores_list = nmt_model.eval_step(batch, return_attn = True);
+    prediction = prediction[0];
+    print('prediction: ', prediction)
+    
+    if attn_scores_list[0] is not None:
+        if attn_scores_list[0][0] is not None:
+            attn_matrix = [x[0].data.cpu().numpy() for x in attn_scores_list];
+            attn_matrix = np.stack(attn_matrix)[:,:, 0]
+            showAttention(sentence, prediction, attn_matrix)

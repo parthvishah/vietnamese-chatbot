f0153922871e5c16ff05c041a88a41e592dca517
Parthvi Shah <shahparthvi22@gmail.com>
2020-11-16 14:30:33 -0500

    Update train_no_attention.py

--- scripts/train_no_attention.py
+++ scripts/train_no_attention.py
@@ -113,7 +113,7 @@ def main():
 		utils.train_model(dataloader_dict, nmt_rnn,num_epochs = num_epochs, saved_model_path = saved_models_dir, enc_type = 'rnn_test')
 	log.info("Total time is: {} min : {} s".format((time.time()-start)//60, (time.time()-start)%60))
 	
-	rnn_len_threshold, rnn_bin_bl = utils.get_binned_bl_score(nmt_rnn, dataset_dict['val'])
+	rnn_len_threshold, rnn_bin_bl = utils.get_binned_bl_score(nmt_rnn, dataset_dict['dev'])
 	#for i in range(10):
 	utils.get_translation(nmt_rnn, 'are hello ? how you', source_lang_obj, use_cuda)
 	

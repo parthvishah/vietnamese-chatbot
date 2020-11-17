cfb75a6bf23a4b71df1c268cc68dca4839350f6f
Anhthy Ngo <an3056@nyu.edu>
2020-11-17 00:50:23 -0500

    new mdeels

--- scripts/nnet_models_new.py
+++ scripts/nnet_models_new.py
@@ -460,11 +460,11 @@ class PositionalEncoding(nn.Module):
 		super(PositionalEncoding, self).__init__()
 		self.register_buffer('pos_table', self._get_sinusoid_encoding_table(n_position, d_hid))
 
-		def _get_sinusoid_encoding_table(self, n_position, d_hid):
+	def _get_sinusoid_encoding_table(self, n_position, d_hid):
 		''' Sinusoid position encoding table '''
 
-			def get_position_angle_vec(position):
-				return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)]
+		def get_position_angle_vec(position):
+			return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)]
 
 		sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)])
 		sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i

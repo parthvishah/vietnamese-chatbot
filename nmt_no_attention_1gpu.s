17f4d109a4e12c3b4c22ab987b5ea22ac36c318b
Parthvi Shah <shahparthvi22@gmail.com>
2020-11-17 19:45:51 -0500

    Update nmt_no_attention_1gpu.s

--- nmt_no_attention_1gpu.s
+++ nmt_no_attention_1gpu.s
@@ -7,9 +7,9 @@
 #SBATCH --mem=64000
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=END
-#SBATCH --mail-user=an3056@nyu.edu
+#SBATCH --mail-user=pss434@nyu.edu
 #SBATCH --job-name="seq2seq_noattn_train"
-#SBATCH --output=/scratch/an3056/nlp_project/outputs/%j.out
+#SBATCH --output=/scratch/pss434/ant/outputs/%j.out
 
 module purge
 module load anaconda3/5.3.1
@@ -17,7 +17,7 @@ module load cuda/10.0.130
 module load gcc/6.3.0
 
 # Replace with your NetID
-NETID=an3056
+NETID=pss434
 source activate nmt_env
 
 # Set project working directory
@@ -37,6 +37,7 @@ RNN_LAYERS=1
 LONGEST_LABEL=1
 GRADIENT_CLIP=0.3
 EPOCHS=10
+SAVE_PLOTS=${PROJECT}/plots
 
 cd ${PROJECT}
 python ./vietnamese-chatbot/scripts/train_no_attention.py \
@@ -52,4 +53,5 @@ python ./vietnamese-chatbot/scripts/train_no_attention.py \
 	--rnn_layers ${RNN_LAYERS} \
 	--longest_label ${LONGEST_LABEL} \
 	--gradient_clip ${GRADIENT_CLIP} \
-	--epochs ${EPOCHS}
+	--epochs ${EPOCHS} \
+	--save_plots ${SAVE_PLOTS}

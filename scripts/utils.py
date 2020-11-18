7463add42e69010b6baf994e3266a17afbbbd647
Anhthy Ngo <an3056@nyu.edu>
2020-11-18 11:24:40 -0500

    utils updatee

--- scripts/utils.py
+++ scripts/utils.py
@@ -126,7 +126,7 @@ def get_encoded_batch(sentence, lang_obj, use_cuda):
 	"""
 
 	sentence = sentence + ' ' + global_variables.EOS_TOKEN;
-	tensor = source_lang_obj.txt2vec(sentence).unsqueeze(0)
+	tensor = lang_obj.txt2vec(sentence).unsqueeze(0)
 
 	device = torch.device('cuda') if use_cuda and torch.cuda.is_available() else torch.device('cpu');
 

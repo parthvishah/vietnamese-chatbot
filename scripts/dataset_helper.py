ad4e2ccef0483988972ab29f3c802b775970761f
Anhthy Ngo <an3056@nyu.edu>
2020-11-23 10:51:28 -0500

    formatting scripts, adding comments

--- scripts/dataset_helper.py
+++ scripts/dataset_helper.py
@@ -50,54 +50,68 @@ EOS_token = global_variables.EOS_token
 UNK_IDX = global_variables.UNK_IDX
 PAD_IDX = global_variables.PAD_IDX
 
+# remove punctuation
 other_punctuations = string.punctuation.replace('!','').replace('.','').replace('?','').replace(',','').replace('-','')
 
+'''
+regex: https://www.regular-expressions.info/tutorial.html
+'''
+
 class Vietnamese(Dataset):
-    def __init__(self, df, val = False):
-        self.df = df
-        self.val = val
+	'''
+	Class that represents a train/validation/test dataset that's readable for PyTorch
+	Note that this class inherits torch.utils.data.Dataset
+	'''
+	def __init__(self, df, val = False):
+		self.df = df
+		self.val = val
 
-    def __len__(self):
-        return len(self.df)
-    def __getitem__(self, idx):
-        english = self.df.iloc[idx,:]['en_idized']
-        viet = self.df.iloc[idx,:]['vi_idized']
-        en_len = self.df.iloc[idx,:]['en_len']
-        vi_len = self.df.iloc[idx,:]['vi_len']
-        if self.val:
-            en_data = self.df.iloc[idx,:]['en_data'].lower()
-            return [viet,english,vi_len,en_len,en_data]
-        else:
-            return [viet,english,vi_len,en_len]
+	def __len__(self):
+		# return number of pairs, triggered when you call len(dataset)
+		return len(self.df)
 
+	def __getitem__(self, idx):
+		# trigged when you call dataset[i]
+		english = self.df.iloc[idx,:]['en_idized']
+		viet = self.df.iloc[idx,:]['vi_idized']
+		en_len = self.df.iloc[idx,:]['en_len']
+		vi_len = self.df.iloc[idx,:]['vi_len']
+		if self.val:
+			en_data = self.df.iloc[idx,:]['en_data'].lower()
+			return [viet,english,vi_len,en_len,en_data]
+		else:
+			return [viet,english,vi_len,en_len]
 
 def normalizeEnString(s):
-    '''
-    format/clean english string
-    '''
-    # remove apostrophe, quote html
-    s = s.replace("&apos", "").replace("&quot","")
-    # remove anything thats not alphanumeric and punctuation
-    s = re.sub(r"[^a-zA-Z,.!?0-9]+", r" ", s)
-    # remove spacing
-    s = re.sub( '\s+', ' ', s).strip()
-    return s
+	'''
+	format/clean english string
+	'''
+	# remove apostrophe, quote html
+	s = s.replace("&apos", "").replace("&quot","")
+	# remove anything thats not alphanumeric and punctuation
+	s = re.sub(r"[^a-zA-Z,.!?0-9]+", r" ", s)
+	# remove spacing
+	s = re.sub( '\s+', ' ', s).strip()
+	return s
 
 
 def normalizeViString(s):
-    '''
-    format/clean viet string
-    '''
-    # remove apostrophe, quote html
-    s = s.replace("&apos", "").replace("&quot","").replace("_","").replace('-','')
-    # remove punctuation
-    s = re.sub(r'[{}]'.format(other_punctuations), '', s)
-    # remove spacing
-    s = re.sub( '\s+', ' ', s).strip()
-    return s
+	'''
+	format/clean viet string
+	'''
+	# remove apostrophe, quote html
+	s = s.replace("&apos", "").replace("&quot","").replace("_","").replace('-','')
+	# remove punctuation
+	s = re.sub(r'[{}]'.format(other_punctuations), '', s)
+	# remove spacing
+	s = re.sub( '\s+', ' ', s).strip()
+	return s
 
 
 def read_dataset(file, lang = None):
+	'''
+	read dataset as pandas df and normalize each sentence
+	'''
     f = open(file)
     list_l = []
     for line in f:
@@ -109,35 +123,51 @@ def read_dataset(file, lang = None):
 
 
 class Lang:
-    def __init__(self, name, minimum_count = 5):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-#         self.index2word = {0: "SOS", 1: "EOS", 2:"UKN",3:"PAD"}
-        self.index2word = ["SOS","EOS","UKN","PAD"]
-        self.n_words = 4  # Count SOS and EOS
-        self.minimum_count = minimum_count
+	'''
+	represent each word in language as one-hot vector.
+	name: name of language
+	word2index: get index from word (word --> index)
+	word2count: get counts for each word in language
+	index2word: unique index per word (index --> word)
+	n_words: number of unique words in language
+	minimum_count: threshold for word count to be added in language
+	'''
+	def __init__(self, name, minimum_count = 5):
+		self.name = name
+		self.word2index = {}
+		self.word2count = {}
+		self.index2word = ["<sos>","<eos>","<unk>","<pad>"]
+		self.n_words = 4  # Count SOS, EOS, PAD, UNK tokens
+		self.minimum_count = minimum_count
 
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word.lower())
-#             if word not in string.punctuation:
-#                 self.addWord(word.lower())
+	def addSentence(self, sentence):
+		'''
+		add sentence to lang obj
+		'''
+		for word in sentence.split(' '):
+			self.addWord(word.lower())
 
-    def addWord(self, word):
-        if word not in self.word2count:
-            self.word2count[word] = 1
-        else:
-            self.word2count[word] += 1
-        if self.word2count[word] >= self.minimum_count:
-            if word not in self.word2index:
-                self.word2index[word] = self.n_words
-    #             self.index2word[self.n_words] = word
-                self.index2word.append(word)
-                self.n_words += 1
 
+	def addWord(self, word):
+		'''
+		add word to language obj and update data structures
+		'''
+		# update word count
+		if word not in self.word2count:
+			self.word2count[word] = 1
+		else:
+			self.word2count[word] += 1
+		# check if the word count is greater than the minimum count
+		if self.word2count[word] >= self.minimum_count:
+			if word not in self.word2index:
+				self.word2index[word] = self.n_words
+				self.index2word.append(word)
+				self.n_words += 1
 
 def split(df):
+	'''
+	create tokenized col for source and target lang
+	'''
     df['en_tokenized'] = df["en_data"].apply(lambda x:x.lower().split( ))
     df['vi_tokenized'] = df['vi_data'].apply(lambda x:x.lower().split( ))
     return df
@@ -145,139 +175,152 @@ def split(df):
 
 
 def token2index_dataset(df,en_lang,vi_lang):
-    for lan in ['en','vi']:
-        indices_data = []
-        if lan=='en':
-            lang_obj = en_lang
-        else:
-            lang_obj = vi_lang
-        for tokens in df[lan+'_tokenized']:
-            index_list = [lang_obj.word2index[token] if token in lang_obj.word2index else UNK_IDX for token in tokens]
-            index_list.append(EOS_token)
-#             index_list.insert(0,SOS_token)
-            indices_data.append(index_list)
-        df[lan+'_idized'] = indices_data
-    return df
+	'''
+	create indicized column for pandas df
+	'''
+	for lan in ['en','vi']:
+		indices_data = []
+		if lan=='en':
+			lang_obj = en_lang
+		else:
+			lang_obj = vi_lang
+		for tokens in df[lan+'_tokenized']:
+			index_list = [lang_obj.word2index[token] if token in lang_obj.word2index else UNK_IDX for token in tokens]
+			index_list.append(EOS_token)
+			indices_data.append(index_list)
+		df[lan+'_idized'] = indices_data
+	return df
 
+def train_val_load(old_lang_obj, path):
+	'''
+	load source and target language objects
+	load pandas df with len, tokenize, indicized cols for src/trg
+	'''
 
-def train_val_load(MAX_LEN, old_lang_obj, path):
-    en_train = read_dataset(path+"/train.tok.en", lang = 'en')
-    en_val = read_dataset(path+"/dev.tok.en", lang = 'en')
-    en_test = read_dataset(path+"/test.tok.en", lang = 'en')
+	# load en and normalize
+	en_train = read_dataset(path+"/iwslt-vi-en/train.tok.en", lang = 'en')
+	en_val = read_dataset(path+"/iwslt-vi-en/dev.tok.en", lang = 'en')
+	en_test = read_dataset(path+"/iwslt-vi-en/test.tok.en", lang = 'en')
 
-    vi_train = read_dataset(path+"/train.tok.vi", lang = 'vi')
-    vi_val = read_dataset(path+"/dev.tok.vi", lang = 'vi')
-    vi_test = read_dataset(path+"/test.tok.vi", lang = 'vi')
+	# load vn and normalize
+	vi_train = read_dataset(path+"/iwslt-vi-en/train.tok.vi", lang = 'vi')
+	vi_val = read_dataset(path+"/iwslt-vi-en/dev.tok.vi", lang = 'vi')
+	vi_test = read_dataset(path+"/iwslt-vi-en/test.tok.vi", lang = 'vi')
 
-    train = pd.DataFrame()
-    train['en_data'] = en_train['data']
-    train['vi_data'] = vi_train['data']
+	# add sentence column for train
+	train = pd.DataFrame()
+	train['en_data'] = en_train['data']
+	train['vi_data'] = vi_train['data']
 
-    val = pd.DataFrame()
-    val['en_data'] = en_val['data']
-    val['vi_data'] = vi_val['data']
+	# add sentence column for val
+	val = pd.DataFrame()
+	val['en_data'] = en_val['data']
+	val['vi_data'] = vi_val['data']
 
-    test = pd.DataFrame()
-    test['en_data'] = en_test['data']
-    test['vi_data'] = vi_test['data']
+	# add sentence column for test
+	test = pd.DataFrame()
+	test['en_data'] = en_test['data']
+	test['vi_data'] = vi_test['data']
 
-    if old_lang_obj:
-        with open(old_lang_obj,'rb') as f:
-            en_lang = pickle.load(f)
-            vi_lang = pickle.load(f)
-    else:
-        en_lang = Lang("en")
-        for ex in train['en_data']:
-            en_lang.addSentence(ex)
+	if old_lang_obj:
+		with open(old_lang_obj,'rb') as f:
+			en_lang = pickle.load(f)
+			vi_lang = pickle.load(f)
+	else:
+		en_lang = Lang("en")
+		for ex in train['en_data']:
+			en_lang.addSentence(ex)
 
-        vi_lang = Lang("vi")
-        for ex in train['vi_data']:
-            vi_lang.addSentence(ex)
+		vi_lang = Lang("vi")
+		for ex in train['vi_data']:
+			vi_lang.addSentence(ex)
 
-        with open("lang_obj.pkl",'wb') as f:
-            pickle.dump(en_lang, f)
-            pickle.dump(vi_lang, f)
+		with open("lang_obj.pkl",'wb') as f:
+			pickle.dump(en_lang, f)
+			pickle.dump(vi_lang, f)
 
-    train = split(train)
-    val = split(val)
-    test = split(test)
+	# get tokenized columns
+	train = split(train)
+	val = split(val)
+	test = split(test)
 
-    train = token2index_dataset(train,en_lang,vi_lang)
-    val = token2index_dataset(val,en_lang,vi_lang)
-    test = token2index_dataset(test,en_lang,vi_lang)
+	# get indicized columns
+	train = token2index_dataset(train,en_lang,vi_lang)
+	val = token2index_dataset(val,en_lang,vi_lang)
+	test = token2index_dataset(test,en_lang,vi_lang)
 
-    train['en_len'] = train['en_idized'].apply(lambda x: len(x))
-    train['vi_len'] = train['vi_idized'].apply(lambda x:len(x))
+	# get sentence lengths for train
+	train['en_len'] = train['en_idized'].apply(lambda x: len(x))
+	train['vi_len'] = train['vi_idized'].apply(lambda x:len(x))
 
-    val['en_len'] = val['en_idized'].apply(lambda x: len(x))
-    val['vi_len'] = val['vi_idized'].apply(lambda x: len(x))
+	# get sentence lengths for val
+	val['en_len'] = val['en_idized'].apply(lambda x: len(x))
+	val['vi_len'] = val['vi_idized'].apply(lambda x: len(x))
 
-    test['en_len'] = test['en_idized'].apply(lambda x: len(x))
-    test['vi_len'] = test['vi_idized'].apply(lambda x: len(x))
+	# get sentence lengths for test
+	test['en_len'] = test['en_idized'].apply(lambda x: len(x))
+	test['vi_len'] = test['vi_idized'].apply(lambda x: len(x))
 
+#     only sentences that have 2 or more words
 #     train = train[np.logical_and(train['en_len']>=2,train['vi_len']>=2)]
 #     train = train[train['vi_len']<=MAX_LEN]
 
 #     val = val[np.logical_and(val['en_len']>=2,val['vi_len']>=2)]
 #     val = val[val['vi_len']<=MAX_LEN]
+	return train, val, test, en_lang, vi_lang
 
-    return train,val,test, en_lang,vi_lang
+def vocab_collate_func(batch, MAX_LEN):
+	'''
+	Customized function for DataLoader that dynamically pads the batch so that all
+	data have the same length
+	'''
 
+	en_data, vi_data, en_len, vi_len = [], [], [], []
+	for datum in batch:
+		# add sent len for each sent in batch
+		en_len.append(datum[3])
+		vi_len.append(datum[2])
 
-def vocab_collate_func(batch):
-    """
-    Customized function for DataLoader that dynamically pads the batch so that all
-    data have the same length
-    """
+	# get max len and source/target len
+	max_batch_length_en = max(en_len)
+	max_batch_length_vi = max(vi_len)
 
-    MAX_LEN_EN = 48
-    MAX_LEN_VI = 48
-    en_data = []
-    vi_data = []
-    en_len = []
-    vi_len = []
-    for datum in batch:
-        en_len.append(datum[3])
-        vi_len.append(datum[2])
-    max_batch_length_en = max(en_len)
-    max_batch_length_vi = max(vi_len)
-    if max_batch_length_en < MAX_LEN_EN:
-        MAX_LEN_EN = max_batch_length_en
-    if max_batch_length_vi < MAX_LEN_VI:
-        MAX_LEN_VI = max_batch_length_vi
-    # padding
-    for datum in batch:
-        if datum[2]>MAX_LEN_VI:
-            padded_vec_s1 = np.array(datum[0])[:MAX_LEN_VI]
-        else:
-            padded_vec_s1 = np.pad(np.array(datum[0]),
-                                pad_width=((0,MAX_LEN_VI - datum[2])),
-                                mode="constant", constant_values=PAD_IDX)
-        if datum[3]>MAX_LEN_EN:
-            padded_vec_s2 = np.array(datum[1])[:MAX_LEN_EN]
-        else:
-            padded_vec_s2 = np.pad(np.array(datum[1]),
-                                pad_width=((0,MAX_LEN_EN - datum[3])),
-                                mode="constant", constant_values=PAD_IDX)
-        en_data.append(padded_vec_s2)
-        vi_data.append(padded_vec_s1)
-    vi_data = np.array(vi_data)
-    en_data = np.array(en_data)
-    vi_len = np.array(vi_len)
-    en_len = np.array(en_len)
-#     sorted_vi_len = np.argsort(-vi_len)
-#     vi_data = vi_data[sorted_vi_len]
-#     en_data = en_data[sorted_vi_len]
-#     vi_len = vi_len[sorted_vi_len]
-#     en_len = en_len[sorted_vi_len]
-#     print(en_len)
-    vi_len[vi_len>MAX_LEN_VI] = MAX_LEN_VI
-    en_len[en_len>MAX_LEN_EN] = MAX_LEN_EN
+	MAX_LEN_VI = np.min([ np.max(vi_len), MAX_LEN])
+	MAX_LEN_EN = np.min([np.max(en_len), MAX_LEN])
 
-    return [torch.from_numpy(vi_data), torch.from_numpy(en_data),
-            torch.from_numpy(vi_len), torch.from_numpy(en_len)]
+	# clip the length to the max length
+	vi_len = np.clip(vi_len, a_min = None, a_max = MAX_LEN_VI)
+	en_len = np.clip(en_len, a_min = None, a_max = MAX_LEN_EN)
 
+	# padding
+	for datum in batch:
+		if datum[2]>MAX_LEN_VI:
+			padded_vec_s1 = np.array(datum[0])[:MAX_LEN_VI]
+		else:
+			padded_vec_s1 = np.pad(np.array(datum[0]), pad_width=((0,MAX_LEN_VI - datum[2])), mode="constant", constant_values=PAD_IDX)
+		if datum[3]>MAX_LEN_EN:
+			padded_vec_s2 = np.array(datum[1])[:MAX_LEN_EN]
+		else:
+			padded_vec_s2 = np.pad(np.array(datum[1]), pad_width=((0,MAX_LEN_EN - datum[3])), mode="constant", constant_values=PAD_IDX)
+		en_data.append(padded_vec_s2)
+		vi_data.append(padded_vec_s1)
+	vi_data = np.array(vi_data)
+	en_data = np.array(en_data)
+	vi_len = np.array(vi_len)
+	en_len = np.array(en_len)
+
+	vi_len[vi_len>MAX_LEN_VI] = MAX_LEN_VI
+	en_len[en_len>MAX_LEN_EN] = MAX_LEN_EN
+
+	return [torch.from_numpy(vi_data), torch.from_numpy(en_data), torch.from_numpy(vi_len), torch.from_numpy(en_len)]
 
 def vocab_collate_func_val(batch):
-    return [torch.from_numpy(np.array(batch[0][0])).unsqueeze(0), torch.from_numpy(np.array(batch[0][1])).unsqueeze(0),
-            torch.from_numpy(np.array(batch[0][2])).unsqueeze(0), torch.from_numpy(np.array(batch[0][3])).unsqueeze(0),batch[0][4]]
+	'''
+	Customized function for DataLoader that dynamically pads the batch so that all data have the same length (for validation)
+	'''
+	vi = torch.from_numpy(np.array(batch[0][0])).unsqueeze(0)
+	en = torch.from_numpy(np.array(batch[0][1])).unsqueeze(0)
+	vi_len = torch.from_numpy(np.array(batch[0][2])).unsqueeze(0)
+	en_len = torch.from_numpy(np.array(batch[0][3])).unsqueeze(0)
+	en_data = batch[0][4]
+	return [vi, en, vi_len, en_len, en_data]
--- scripts/utils.py
+++ scripts/utils.py
@@ -12,10 +12,9 @@ import numpy as np
 from collections import namedtuple
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from googletrans import Translator
+
 
 import global_variables
-# import nmt_dataset
 import dataset_helper
 import nnet_models
 from args import args, check_args
@@ -28,10 +27,7 @@ This .py file contains the following functions:
 	get_binned_bl_score(nmt_model, val_dataset, location, batchSize)
 	showAttention(input_sentence, output_words, attentions)
 	get_encoded_batch(sentence, lang_obj, use_cuda)
-	get_translation(nmt_model, sentence, lang_obj, use_cuda, source_name = 'vi', target_name = 'en')
 """
-
-
 
 def get_full_filepath(path, nn_type):
 	'''
@@ -48,65 +44,38 @@ def save_models(model, path, nn_type):
 		os.makedirs(path)
 	filename = "lstm_w_att_"+nn_type+".pth"
 	torch.save(model, os.path.join(path, filename))
-#
-# def train_model(dataloader, nmt, num_epochs=50, val_every=1, saved_model_path = '.', enc_type ='rnn'):
-# 	'''
-# 	nmt training loop
-# 	'''
-# 	best_bleu = -1;
-# 	for epoch in range(num_epochs):
-#
-# 		start = time.time()
-# 		running_loss = 0
-#
-# 		print('Epoch: [{}/{}]'.format(epoch, num_epochs));
-#
-# 		for i, data in enumerate(dataloader['train']):
-# 			_, curr_loss = nmt.train_step(data);
-# 			running_loss += curr_loss
-#
-# 		epoch_loss = running_loss / len(dataloader['train'])
-#
-# 		print("epoch {} loss = {}, time = {}".format(epoch, epoch_loss, time.time() - start))
-#
-# 		sys.stdout.flush()
-#
-# 		if epoch%val_every == 0:
-# 			val_bleu_score = nmt.get_bleu_score(dataloader['dev']);
-# 			print('validation bleu: ', val_bleu_score)
-# 			sys.stdout.flush()
-#
-# 			nmt.scheduler_step(val_bleu_score);
-#
-# 			if val_bleu_score > best_bleu:
-# 				best_bleu = val_bleu_score
-# 				save_models(nmt, saved_model_path, enc_type);
-# 			log.info(f"epoch {epoch} | loss {epoch_loss} | time = {time.time() - start} | validation bleu = {val_bleu_score}")
-#
-# 		print('='*50)
-#
-# 	print("Training completed. Best BLEU is {}".format(best_bleu))
 
-def get_binned_bl_score(nmt_model, val_dataset, location, batchSize):
+def get_binned_bl_score(encoder, decoder, val_dataset, min_len = 0, max_len = 30):
+	'''
+	return plot for binned bleu scores
+	'''
 
-	len_threshold = np.arange(0, 31, 5)
-	bin_bl_score = np.zeros(len(len_threshold));
+	# set bins
+	len_threshold = np.arange(min_len, max_len + 1, 5)
+	# intiate bleu score list
+	bin_bl_score = np.zeros(len(len_threshold))
 
-	for i in range(1, len(len_threshold)):
-		min_len = len_threshold[i-1]
-		max_len = len_threshold[i]
+	for i in notebook.tqdm(range(1, len(len_threshold)), total = len(len_threshold)-1):
+		# set lower and upper bound buckets
+		lower_bound = len_threshold[i-1]
+		upper_bound = len_threshold[i]
 
+		# subset val df
 		temp_dataset = copy.deepcopy(val_dataset);
-		temp_dataset.main_df = temp_dataset.main_df[(temp_dataset.main_df['source_len'] > min_len) & (temp_dataset.main_df['source_len'] <= max_len)];
-		#temp_loader = DataLoader(temp_dataset, batch_size = batchSize, collate_fn = partial(nmt_dataset.vocab_collate_func, MAX_LEN=100), shuffle = True, num_workers=0)
-		temp_loader = DataLoader(temp_dataset, batch_size = batchSize, collate_fn = partial(dataset_helper.vocab_collate_func), shuffle = True, num_workers=0)
+		temp_dataset.df = temp_dataset.df[(temp_dataset.df['vi_len'] > lower_bound) & (temp_dataset.df['vi_len'] <= upper_bound)];
 
-		bin_bl_score[i] = nmt_model.get_bleu_score(temp_loader);
+		# val dataloader
+		temp_loader = DataLoader(temp_dataset, batch_size = 1, collate_fn = vocab_collate_func_val, shuffle = False, num_workers=0)
 
+		# evaluate
+		bin_bl_score[i], _, _, _ = validation_beam_search(encoder, decoder, temp_loader,en_lang,\
+		vi_lang, 'no_attention',3,verbose=False)
+		
+	# plot bleu score vs. sent lengh
 	len_threshold = len_threshold[1:]
 	bin_bl_score = bin_bl_score[1:]
 	fig = plt.figure()
-	plt.plot(len_threshold, bin_bl_score, 'x-')
+	plt.plot(len_threshold, bin_bl_score, '+-')
 	plt.ylim(0, np.max(bin_bl_score)+1)
 	plt.xlabel('sentence length')
 	plt.ylabel('bleu score')
@@ -116,53 +85,37 @@ def get_binned_bl_score(nmt_model, val_dataset, location, batchSize):
 
 	return len_threshold, bin_bl_score, fig
 
-def showAttention(input_sentence, output_words, attentions):
-	# Set up figure with colorbar
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	cax = ax.matshow(attentions, cmap='bone', aspect='auto')
-	fig.colorbar(cax)
 
-	# Set up axes
-	ax.set_xticklabels([''] + input_sentence.split(' ') + [global_variables.EOS_TOKEN], rotation=90)
-	ax.set_yticklabels([''] + output_words.split(' ')+ [global_variables.EOS_TOKEN]);
-
-	# Show label at every tick
-	ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-	ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-
-	plt.show()
-
-def get_encoded_batch(sentence, lang_obj, use_cuda):
-	"""
-	accepts only bsz = 1.
-	input: one sentence as a string
-	output: named tuple with vector and length
-	"""
-
-	sentence = sentence + ' ' + global_variables.EOS_TOKEN;
-	tensor = lang_obj.txt2vec(sentence).unsqueeze(0)
-
-	device = torch.device('cuda') if use_cuda and torch.cuda.is_available() else torch.device('cpu');
-
-	named_returntuple = namedtuple('namedtuple', ['text_vecs', 'text_lens', 'label_vecs', 'label_lens', 'use_packed'])
-	return_tuple = named_returntuple( tensor.to(device), torch.from_numpy(np.array([tensor.shape[-1]])).to(device), None, None, False);
-
-	return return_tuple
-
-def get_translation(nmt_model, sentence, lang_obj, use_cuda, source_name = 'vi', target_name = 'en'):
-	from googletrans import Translator
-	translator = Translator()
-	print('source: ', sentence)
-	batch = get_encoded_batch(sentence, lang_obj, use_cuda);
-	prediction, attn_scores_list = nmt_model.eval_step(batch, return_attn = True);
-	prediction = prediction[0];
-	print('prediction: ', prediction)
-	print('GT on sentence (src->tgt): ', translator.translate(sentence, src = source_name, dest = target_name).text)
-	print('GT on prediction (tgt->src): ', translator.translate(prediction, src = target_name, dest = source_name).text)
-
-	if attn_scores_list[0] is not None:
-		if attn_scores_list[0][0] is not None:
-			attn_matrix = [x[0].data.cpu().numpy() for x in attn_scores_list];
-			attn_matrix = np.stack(attn_matrix)[:,:, 0]
-			showAttention(sentence, prediction, attn_matrix)
+# def showAttention(input_sentence, output_words, attentions):
+# 	# Set up figure with colorbar
+# 	fig = plt.figure()
+# 	ax = fig.add_subplot(111)
+# 	cax = ax.matshow(attentions, cmap='bone', aspect='auto')
+# 	fig.colorbar(cax)
+#
+# 	# Set up axes
+# 	ax.set_xticklabels([''] + input_sentence.split(' ') + [global_variables.EOS_TOKEN], rotation=90)
+# 	ax.set_yticklabels([''] + output_words.split(' ')+ [global_variables.EOS_TOKEN]);
+#
+# 	# Show label at every tick
+# 	ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+# 	ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+#
+# 	plt.show()
+#
+# def get_encoded_batch(sentence, lang_obj, use_cuda):
+# 	"""
+# 	accepts only bsz = 1.
+# 	input: one sentence as a string
+# 	output: named tuple with vector and length
+# 	"""
+#
+# 	sentence = sentence + ' ' + global_variables.EOS_TOKEN;
+# 	tensor = lang_obj.txt2vec(sentence).unsqueeze(0)
+#
+# 	device = torch.device('cuda') if use_cuda and torch.cuda.is_available() else torch.device('cpu');
+#
+# 	named_returntuple = namedtuple('namedtuple', ['text_vecs', 'text_lens', 'label_vecs', 'label_lens', 'use_packed'])
+# 	return_tuple = named_returntuple( tensor.to(device), torch.from_numpy(np.array([tensor.shape[-1]])).to(device), None, None, False);
+#
+# 	return return_tuple

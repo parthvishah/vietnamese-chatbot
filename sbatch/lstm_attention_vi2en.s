a350442813ca3d8a668da41ccdae5eaa027a1920
Ant Ngo <an3056@nyu.edu>
2020-11-24 16:40:28 -0500

    update

--- sbatch/lstm_attention_vi2en.s
+++ sbatch/lstm_attention_vi2en.s
@@ -7,9 +7,9 @@
 #SBATCH --mem=64000
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=END
-#SBATCH --mail-user=ltk224@nyu.edu
+#SBATCH --mail-user=an3056@nyu.edu
 #SBATCH --job-name="testing"
-#SBATCH --output=/scratch/ltk224/nlp_project/outputs/%j.out
+#SBATCH --output=/scratch/an3056/nlp_project/outputs/%j.out
 
 module purge
 module load anaconda3/5.3.1
@@ -17,7 +17,7 @@ module load cuda/10.0.130
 module load gcc/6.3.0
 
 # Replace with your NetID
-NETID=ltk224
+NETID=an3056
 source activate nmt_env
 
 # Set project working directory
@@ -28,14 +28,14 @@ STUDY_NAME=attn_lstm_64bs_512em_3beam_2layer
 SAVE_DIR=${PROJECT}/saved_models
 DATA_DIR=${PROJECT}/nmt-vi-en/data/interim/iwslt15-en-vn
 PLOT_DIR=${PROJECT}/plots
-BATCH_SIZE=64
+BATCH_SIZE=128
 LR=0.25
 SEED=42
 SOURCE_NAME='vi'
 TARGET_NAME='en'
 ENC_EMB=512
 ENC_HIDDEN=512
-ENC_LAYERS=2
+ENC_LAYERS=1
 RNN_TYPE='lstm'
 DEC_EMB=512
 DEC_HIDDEN=1024
